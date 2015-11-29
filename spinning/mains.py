624663597e2474408783aff45204a4aa17fb81ab
walid <walid.sa3d@gmail.com>
2015-11-29 12:03:04 +0100

    remove progressbar

--- spinning/mains.py
+++ spinning/mains.py
@@ -4,49 +4,48 @@
 from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import absolute_import
-from future.standard_library import install_aliases
-install_aliases()
 import sys
 import time
-import blessings
 import os
+import itertools
+from termcolor import colored
 
-term = blessings.Terminal()
-
-shapes = {
+frames = {
     'tetris': ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'],
     'swords': ['-', '/', '\\'],
     'circles': ['.', 'o', 'O', '°', 'O', 'o', '.'],
     'hourglass': ['⏳', '⌛'],
     'moons': ["◐", "◓", "◑", "◒"],
-    'progressbar': ['█▒▒▒▒', '███▒▒', '█████'],
     'histogram': ['▁', '▃', '▄', '▅', '▆', '▇', '█', '▇', '▆', '▅', '▄', '▃', '▁'],
     'clock': ['🕛', '🕐', '🕑', '🕒', '🕓', '🕔', '🕕', '🕖', '🕗', '🕘', '🕙', '🕚']
 }
 
 
-class spin:
+class Spinner:
 
-    def __init__(self, sec=5, color='cyan', form='tetris'):
-        self.display = {
-            'cyan': term.cyan,
-            'red': term.red,
-            'magenta': term.magenta,
-            'green': term.green,
-            'yellow': term.yellow,
-            'white': term.white
-        }
+    def __init__(self, sec=5, color='cyan', shape='tetris'):
+        self.shape = shape
+        self.color = color
+        self.sec = sec
 
-        def spin():
-            while True:
-                for x in shapes[form]:
-                    yield self.display.get(color, term.cyan)(x)
+    def cursor_on(self):
+        os.system('setterm -cursor on')
 
-        spinner = spin()
+    def cursor_off(self):
         os.system('setterm -cursor off')
-        for _ in range(sec*10):
-            sys.stdout.write(next(spinner))
+
+    def spin(self):
+        shape = frames[self.shape]
+        frame_gen = itertools.cycle(shape)
+        self.cursor_off()
+        for _ in xrange(self.sec*10):
+            frame = colored(next(frame_gen), self.color)
+            sys.stdout.write(frame)
             sys.stdout.flush()
             time.sleep(0.1)
             sys.stdout.write('\b')
-        os.system('setterm -cursor on')
+        self.cursor_on()
+
+spinner = Spinner(color='blue', shape='clock')
+while True:
+    spinner.spin()
